0a2d477fc34545c2dac3672448114e8f8a15bdd2
asc1326777 <ascll1326@gmail.com>
2026-03-09 17:46:48 +0900

    1주차 과제.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.a_img;
+            pictureBox1.Location = new Point(2, 28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(366, 376);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseEnter += pictureBox1_MouseEnter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 19.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(404, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(328, 71);
+            label1.TabIndex = 1;
+            label1.Text = "이름: 안성철";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 10F);
+            label2.Location = new Point(404, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(248, 37);
+            label2.TabIndex = 2;
+            label2.Text = "   소속: 컴퓨터학부";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 10F);
+            label3.Location = new Point(404, 213);
+            label3.Name = "label3";
+            label3.Size = new Size(379, 37);
+            label3.TabIndex = 3;
+            label3.Text = "이메일: ascll1326@gmail.com";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(224, 224, 224);
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(64, 64, 64);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(461, 363);
+            button1.Name = "button1";
+            button1.Size = new Size(238, 51);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaptionText;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderColor = Color.Black;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(224, 224, 224);
+            button2.FlatAppearance.MouseOverBackColor = Color.DimGray;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(461, 294);
+            button2.Name = "button2";
+            button2.Size = new Size(238, 51);
+            button2.TabIndex = 5;
+            button2.Text = "GitHub";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.FromArgb(224, 224, 224);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(116, 407);
+            button3.Margin = new Padding(0);
+            button3.Name = "button3";
+            button3.Size = new Size(139, 36);
+            button3.TabIndex = 6;
+            button3.Text = "사진 변경";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,46 @@ namespace hw1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/ascll1326-lgtm",
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.as_img;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
         }
     }
 }
